792a6e77b9a87f35e90f6e2e3959f11812b8c1cf
Brandon2Black <brandonb627@nycstudents.net>
2025-10-24 12:41:38 -0400

    .

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+
+    public GameObject[] ghostPrefabs;
+    public int ghostIndex;
+
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    private float startDelay = 2;
+    private float spawnInterval = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+     InvokeRepeating("SpawnRandomGhost", startDelay, Random.Range(spawnInterval, 5));
     }
+ 
 
     // Update is called once per frame
     void Update()
     {
-        
+     
+    }
+
+    void SpawnRandomGhost()
+    {
+          Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0,spawnPosZ);
+           int ghostIndex = Random.Range(0, ghostPrefabs.Length);
+      Instantiate(ghostPrefabs[ghostIndex], spawnPos, ghostPrefabs[ghostIndex].transform.rotation);
     }
 }
+
